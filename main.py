ee46a70a041060fcc8bff92e8ccf3dff0a578f5d
Shaked Caspi <caspi5032@gmail.com>
2023-01-04 17:30:29 +0200

    add games and coaches csv files and update main

--- main.py
+++ main.py
@@ -2,46 +2,79 @@ import requests
 import pandas as pd
 
 API_TOKEN = "Bearer n6ltTo7OHNOdFrWhoNnGVM25QQRg3GTwyC7UnBTErHL6oFaIhlaD4OqbW42KMOai"
+BASE_URL = "https://api.collegefootballdata.com"
+HEADERS = {
+    'Authorization': API_TOKEN
+}
+SEASONS = [_ for _ in range(2000, 2023)]
+
 
 def get_teams():
-	df = pd.DataFrame(columns=['id', 'school', 'abbreviation', 'mascot'])
-	url = "https://api.collegefootballdata.com/teams"
-	headers = {
-		'Authorization' : API_TOKEN
-	}
+    columns = ['id', 'school', 'abbreviation', 'mascot']
+    df = pd.DataFrame(columns=columns)
+    url = BASE_URL + "/teams"
 
-	response = requests.request("GET", url, headers=headers).json()
-	for team in response:
-		row = list(map(team.get, ['id', 'school', 'abbreviation', 'mascot']))
-		tmp_df = pd.DataFrame([row], columns = ['id', 'school', 'abbreviation', 'mascot'])
-		df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
+    response = requests.request("GET", url, headers=HEADERS).json()
+    for team in response:
+        row = list(map(team.get, columns))
+        tmp_df = pd.DataFrame([row], columns=columns)
+        df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
 
-	df.to_csv('teams.csv', index=False)
+    df.to_csv('teams.csv', index=False)
 
 
 def get_games():
-	seasons = [year for year in range(2000, 2023)]
-	columns = ['id', 'season', 'home_id', 'home_team', 'away_id', 'away_team']
-	df = pd.DataFrame(columns=columns)
+    columns = ['id', 'season', 'home_id', 'home_team', 'away_id', 'away_team']
 
-	for season in seasons:
-		url = f"https://api.collegefootballdata.com/games?year={season}"
-		headers = {
-			'Authorization' : API_TOKEN
-		}
+    df = pd.DataFrame(columns=columns)
 
-		response = requests.request("GET", url, headers=headers).json()
-		for game in response:
-			row = list(map(game.get, columns))
-			tmp_df = pd.DataFrame([row], columns = columns)
-			df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
-
-	df.to_csv('games.csv', index=False)
+    for season in SEASONS:
+        url = BASE_URL + f"/games?year={season}"
+        response = requests.request("GET", url, headers=HEADERS).json()
+        for game in response:
+            row = list(map(game.get, columns))
+            tmp_df = pd.DataFrame([row], columns=columns)
+            df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
+        print(f"DONE - {season}")
+    df.to_csv('games.csv', index=False)
 
 
+def get_coaches():
+    columns = ['first_name', 'last_name', 'year', 'wins', 'losses', 'ties', 'school']
+    df = pd.DataFrame(columns=columns)
+    start_year = 2000
+    end_year = 2022
+    url = BASE_URL + f"/coaches?minYear={start_year}&maxYear={end_year}"
+    responses = requests.request("GET", url, headers=HEADERS).json()
+    for response in responses:
+        metadata_columns = ['first_name', 'last_name']
+        metadata_values = list(map(response.get, metadata_columns))
+        seasons = response["seasons"]
+        for season in seasons:
+            per_season_column = ['year', 'wins', 'losses', 'ties', 'school']
+            per_season_values = list(map(season.get, per_season_column))
+            tmp_df = pd.DataFrame([metadata_values + per_season_values], columns=columns)
+            df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
+            print(tmp_df)
+    df.to_csv('coaches.csv', index=False)
 
-# Press the green button in the gutter to run the script.
+
+def get_stats():
+    columns = ['team', 'stat_name', 'stat_value', 'season']
+    df = pd.DataFrame(columns=columns)
+
+    for season in SEASONS:
+        url = BASE_URL + f"/stats/season?year={season}"
+        response = requests.request("GET", url, headers=HEADERS).json()
+        for game in response:
+            row = list(map(game.get, columns))
+            tmp_df = pd.DataFrame([row], columns=columns)
+            df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
+        print(f"DONE - {season}")
+    df.to_csv('stats.csv', index=False)
+
+
 if __name__ == '__main__':
-    get_games()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # get_games()
+    # get_coaches()
+    get_stats()

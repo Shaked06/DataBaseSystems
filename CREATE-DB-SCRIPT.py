7079f569992e09d65093305ac81fa31155ac28e9
tomerfatael <73110840+tomerfatael@users.noreply.github.com>
2023-01-05 10:20:28 +0200

    last update from yesterday

--- CREATE-DB-SCRIPT.py
+++ CREATE-DB-SCRIPT.py
@@ -14,12 +14,24 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-def create_table_from_csv(file_name, table_name, table_columns):
-    query = f"CREATE TABLE {table_name} {table_columns}"
-    cursor.execute(query)
+def create_table_from_csv(table_name, table_columns):
+    # query = f"CREATE TABLE {table_name}{table_columns}"
+    # cursor.execute(query)
 
     df = pd.read_csv(f"./data/{table_name}.csv")
+    s = ("%s," * 6)[:-1]
+    i = 0
+    for index, row in df.iterrows():
+        if i > 3:
+            break
+        i += 1
 
 
+        # row_values = ','.join([str(_) for _ in row])
+        # query = f"INSERT INTO {table_name} VALUES ({row_values})"
+        sql = f"INSERT INTO {table_name} VALUES ({s})"
+        cursor.execute(sql, tuple(row))
+        # cursor.execute(query)
+        db.commit()
 
-create_table_from_csv("games.csv", "games", "test()")
+create_table_from_csv("games", "(id VARCHAR(255), season VARCHAR(255), home_id VARCHAR(255), home_team VARCHAR(255), away_id VARCHAR(255), away_tean VARCHAR(255))")

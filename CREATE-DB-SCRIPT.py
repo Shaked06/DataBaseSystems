8464dc8a6a4a25dfbeeed945d89858d4a1b37f71
tomerfatael <tomerfatael@mail.tau.ac.il>
2023-01-06 16:33:51 +0200

    created teams and games tables

--- CREATE-DB-SCRIPT.py
+++ CREATE-DB-SCRIPT.py
@@ -11,27 +11,46 @@ db = mysql.connector.connect(
     password=DB_PASSWORD,
     database=DB_NAME
     )
-
 cursor = db.cursor()
 
-def create_table_from_csv(table_name, table_columns):
-    # query = f"CREATE TABLE {table_name}{table_columns}"
-    # cursor.execute(query)
+def create_table(table_name, table_columns):
+    query = f"CREATE TABLE {table_name}{table_columns}"
+    cursor.execute(query)
+    #need to commit?
 
+
+def insert_values_into_table_from_csv(table_name, number_of_columns):
     df = pd.read_csv(f"./data/{table_name}.csv")
-    s = ("%s," * 6)[:-1]
-    i = 0
+    s = ("%s," * number_of_columns)[:-1]
+
     for index, row in df.iterrows():
-        if i > 3:
-            break
-        i += 1
-
-
-        # row_values = ','.join([str(_) for _ in row])
-        # query = f"INSERT INTO {table_name} VALUES ({row_values})"
-        sql = f"INSERT INTO {table_name} VALUES ({s})"
-        cursor.execute(sql, tuple(row))
-        # cursor.execute(query)
+        query = f"INSERT INTO {table_name} VALUES ({s})"
+        cursor.execute(query, tuple(row))
         db.commit()
 
-create_table_from_csv("games", "(id VARCHAR(255), season VARCHAR(255), home_id VARCHAR(255), home_team VARCHAR(255), away_id VARCHAR(255), away_tean VARCHAR(255))")
+
+
+
+try:
+    # create_table("teams", "(id INT PRIMARY KEY,"
+    #                       " school VARCHAR(255),"
+    #                       " mascot VARCHAR(255),"
+    #                       " abbreviation VARCHAR(255))")
+    # insert_values_into_table_from_csv("teams", 4)
+
+    # create_table("games", "(id INT PRIMARY KEY,"
+    #                       " season INT,"
+    #                       " home_id INT,"
+    #                       " home_team VARCHAR(255),"
+    #                       " away_id INT,"
+    #                       " away_team VARCHAR(255),"
+    #                       " FOREIGN KEY (home_id) REFERENCES teams(id),"
+    #                       " FOREIGN KEY (away_id) REFERENCES teams(id))")
+    # insert_values_into_table_from_csv("games", 6)
+
+    # create_table("roster", )
+
+except Exception as e:
+    print("Failed to create table: " + str(e))
+    db.rollback()
+

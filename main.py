ba081573582a15075a64b8a6a9a53e88b94417ab
Shaked Caspi <caspi5032@gmail.com>
2023-01-08 01:44:58 +0200

    change data files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 import os
-import re
 
 API_TOKEN = "Bearer n6ltTo7OHNOdFrWhoNnGVM25QQRg3GTwyC7UnBTErHL6oFaIhlaD4OqbW42KMOai"
 BASE_URL = "https://api.collegefootballdata.com"
@@ -12,13 +11,15 @@ SEASONS = [_ for _ in range(2020, 2023)]
 
 
 def get_teams():
-    columns = ['id', 'school', 'abbreviation', 'mascot']
+    columns = ['id', 'school', 'abbreviation', 'mascot', 'venue_id']
     df = pd.DataFrame(columns=columns)
     url = BASE_URL + "/teams"
 
     response = requests.request("GET", url, headers=HEADERS).json()
     for team in response:
-        row = list(map(team.get, columns))
+        tmp_columns = ['id', 'school', 'abbreviation', 'mascot']
+        row = list(map(team.get, tmp_columns))
+        row.append(team["location"]["venue_id"])
         tmp_df = pd.DataFrame([row], columns=columns)
         df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
 
@@ -138,11 +139,26 @@ def get_plays_20_22():
     print("DONE")
 
 
+def get_venues():
+    columns = ['id', 'name', 'year', 'capacity', 'grass', 'city', 'state']
+    df = pd.DataFrame(columns=columns)
+    url = BASE_URL + "/venues"
+
+    response = requests.request("GET", url, headers=HEADERS).json()
+    for team in response:
+        row = list(map(team.get, columns))
+        tmp_df = pd.DataFrame([row], columns=columns)
+        df = pd.concat([df, tmp_df], axis=0, ignore_index=True)
+
+    df.to_csv('data/venues.csv', index=False)
+
+
+
 if __name__ == '__main__':
-    # get_teams()
+    get_teams()
     # get_games()
     # get_coaches()
     # get_stats()
     # get_plays()
     # get_plays_20_22()
-    pass
+    # get_venues()

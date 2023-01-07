100a457256eaf8480b540c37bfed46568ad5900a
tomerfatael <tomerfatael@mail.tau.ac.il>
2023-01-07 14:39:52 +0200

    added index and tables

--- CREATE-DB-SCRIPT.py
+++ CREATE-DB-SCRIPT.py
@@ -10,13 +10,14 @@ db = mysql.connector.connect(
     user=DB_NAME,
     password=DB_PASSWORD,
     database=DB_NAME
-    )
+)
 cursor = db.cursor()
+
 
 def create_table(table_name, table_columns):
     query = f"CREATE TABLE {table_name}{table_columns}"
     cursor.execute(query)
-    #need to commit?
+    # need to commit?
 
 
 def insert_values_into_table_from_csv(table_name, number_of_columns):
@@ -28,7 +29,9 @@ def insert_values_into_table_from_csv(table_name, number_of_columns):
         cursor.execute(query, tuple(row))
         db.commit()
 
-
+def create_index(table_name, column_name):
+    query = f"CREATE INDEX {column_name}Index ON {table_name}({column_name})"
+    cursor.execute(query)
 
 
 try:
@@ -37,20 +40,39 @@ try:
     #                       " mascot VARCHAR(255),"
     #                       " abbreviation VARCHAR(255))")
     # insert_values_into_table_from_csv("teams", 4)
+    # create_index("teams", "school")
 
     # create_table("games", "(id INT PRIMARY KEY,"
     #                       " season INT,"
     #                       " home_id INT,"
-    #                       " home_team VARCHAR(255),"
     #                       " away_id INT,"
-    #                       " away_team VARCHAR(255),"
     #                       " FOREIGN KEY (home_id) REFERENCES teams(id),"
     #                       " FOREIGN KEY (away_id) REFERENCES teams(id))")
-    # insert_values_into_table_from_csv("games", 6)
+    # insert_values_into_table_from_csv("games", 4)
 
-    # create_table("roster", )
+    # create_table("coaches", "(first_name VARCHAR(255),"
+    #                         "second_name VARCHAR(255),"
+    #                         "year INT,"
+    #                         "wins INT,"
+    #                         "losses INT,"
+    #                         "ties INT,"
+    #                         "school VARCHAR(255))")
+    # insert_values_into_table_from_csv("coaches", 7)
+    # query = "ALTER TABLE coaches ADD id INT PRIMARY KEY AUTO_INCREMENT"
+    # query = "ALTER TABLE coaches ADD FOREIGN KEY (school) REFERENCES teams(school)"
+    # cursor.execute(query)
+
+    # create_table("stats", "(team VARCHAR(255),"
+    #                       "stat_name VARCHAR(255),"
+    #                       "stat_value INT,"
+    #                       "season VARCHAR(255))")
+    #                       # "FOREIGN KEY (team) REFERENCES teams(school))") #referenced columns must be indexed
+    # insert_values_into_table_from_csv("stats", 4)
+    # query = "ALTER TABLE stats ADD id INT PRIMARY KEY AUTO_INCREMENT"
+    # cursor.execute(query)
+    query = "ALTER TABLE stats ADD FOREIGN KEY (team) REFERENCES teams(school)"
+    cursor.execute(query)
 
 except Exception as e:
-    print("Failed to create table: " + str(e))
+    print("Failed to create table due to error: " + str(e))
     db.rollback()
-

1fb425370b4a643bada951cfccdf8fce48b9d9ba
tomerfatael <tomerfatael@mail.tau.ac.il>
2023-01-10 23:51:42 +0200

    added index and tables

--- CREATE-DB-SCRIPT.py
+++ CREATE-DB-SCRIPT.py
@@ -29,17 +29,27 @@ def insert_values_into_table_from_csv(table_name, number_of_columns):
         cursor.execute(query, tuple(row))
         db.commit()
 
+
 def create_index(table_name, column_name):
     query = f"CREATE INDEX {column_name}Index ON {table_name}({column_name})"
     cursor.execute(query)
 
-
 try:
+    # create_table("venues", "(id INT PRIMARY KEY,"
+    #                        "name VARCHAR(255),"
+    #                        "capacity INT,"
+    #                        "grass INT,"
+    #                        "city VARCHAR(255),"
+    #                        "state VARCHAR(255))")
+    # insert_values_into_table_from_csv("venues", 6)
+
     # create_table("teams", "(id INT PRIMARY KEY,"
     #                       " school VARCHAR(255),"
     #                       " mascot VARCHAR(255),"
-    #                       " abbreviation VARCHAR(255))")
-    # insert_values_into_table_from_csv("teams", 4)
+    #                       " abbreviation VARCHAR(255),"
+    #                       " venue_id INT,"
+    #                       "FOREIGN KEY (venue_id) REFERENCES venues(id))")
+    # insert_values_into_table_from_csv("teams", 5)
     # create_index("teams", "school")
 
     # create_table("games", "(id INT PRIMARY KEY,"
@@ -66,12 +76,12 @@ try:
     #                       "stat_name VARCHAR(255),"
     #                       "stat_value INT,"
     #                       "season VARCHAR(255))")
-    #                       # "FOREIGN KEY (team) REFERENCES teams(school))") #referenced columns must be indexed
     # insert_values_into_table_from_csv("stats", 4)
     # query = "ALTER TABLE stats ADD id INT PRIMARY KEY AUTO_INCREMENT"
     # cursor.execute(query)
-    query = "ALTER TABLE stats ADD FOREIGN KEY (team) REFERENCES teams(school)"
-    cursor.execute(query)
+    # query = "ALTER TABLE stats ADD FOREIGN KEY (team) REFERENCES teams(school)"
+    # cursor.execute(query)
+
 
 except Exception as e:
     print("Failed to create table due to error: " + str(e))
